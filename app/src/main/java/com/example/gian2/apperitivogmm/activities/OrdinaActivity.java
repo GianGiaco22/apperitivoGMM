7c39edf500c87ce8f64b5fe6ecfc88fd6a10d4a3
melissa <melissa.palazzo@sudenti.unipd.it>
2018-10-25 15:28:04 +0100

    Modificate stringhe menù

--- app/src/main/java/com/example/gian2/apperitivogmm/activities/OrdinaActivity.java
+++ app/src/main/java/com/example/gian2/apperitivogmm/activities/OrdinaActivity.java
@@ -52,27 +52,27 @@ public class OrdinaActivity extends AppCompatActivity implements View.OnClickLis
             menu[i]=new Pietanza();
         }
         menu[0].setCategoria("primo");
-        menu[0].setDescrizione("spaghetti con vongole veraci e prezzemolo");
-        menu[0].setNome("Spaghetti con le vongole");
+        menu[0].setDescrizione("Spaghetti con vongole veraci, prezzemolo, un filo di olio");
+        menu[0].setNome("Spaghetti alle vongole");
         menu[0].setPrezzo(9.5f);
         menu[1].setCategoria("primo");
-        menu[1].setDescrizione("bigoli al ragù d anatra con formaggio grana");
-        menu[1].setNome("Bigoli al ragù d'anatra");
+        menu[1].setDescrizione("Bigoli al ragù d'anatra con formaggio grana");
+        menu[1].setNome("Bigoli all'anatra");
         menu[1].setPrezzo(9.5f);
         menu[2].setCategoria("primo");
-        menu[2].setDescrizione("gnocchi fatti in casa con ragù di manzo");
+        menu[2].setDescrizione("Gnocchi di patate fatti in casa con ragù di manzo");
         menu[2].setNome("Gnocchi al ragù");
         menu[2].setPrezzo(9f);
         menu[3].setCategoria("primo");
-        menu[3].setDescrizione("riso con vongole, gamberetti e pescato del giorno");
-        menu[3].setNome("Riso con pesce");
+        menu[3].setDescrizione("Riso con vongole, gamberetti e pescato del giorno");
+        menu[3].setNome("Risotto di pesce");
         menu[3].setPrezzo(13f);
         menu[4].setCategoria("primo");
-        menu[4].setDescrizione("bigoli con ragù di manzo");
+        menu[4].setDescrizione("Bigoli con ragù di manzo");
         menu[4].setNome("Bigoli al ragù");
         menu[4].setPrezzo(9f);
         menu[5].setCategoria("primo");
-        menu[5].setDescrizione("gnocchi con salsa di pomodoro");
+        menu[5].setDescrizione("Gnocchi con passata di pomodoro");
         menu[5].setNome("Gnocchi al pomodoro");
         menu[5].setPrezzo(8f);
         for(int i=0;i<6;i++){
@@ -83,45 +83,45 @@ public class OrdinaActivity extends AppCompatActivity implements View.OnClickLis
         menu[1].setNome("Tartare di verdure");
         menu[1].setPrezzo(10f);
         menu[2].setCategoria("secondo");
-        menu[2].setDescrizione("pescata del giorno con contorno di patate lesse");
+        menu[2].setDescrizione("Grigliata di pesce fresco con patate lesse");
         menu[2].setNome("Grigliata mista di pesce");
         menu[2].setPrezzo(18f);
         menu[3].setCategoria("secondo");
-        menu[3].setDescrizione("osetti,salsiccia,pancetta e polenta di contorno");
+        menu[3].setDescrizione("Ossetti,salsiccia,pancetta e polenta");
         menu[3].setNome("Grigliata mista di carne");
         menu[3].setPrezzo(16f);
         menu[4].setCategoria("secondo");
-        menu[4].setDescrizione("tagliata di tonno con verdure di contorno di stagione");
+        menu[4].setDescrizione("Tagliata di tonno con verdure di stagione");
         menu[4].setNome("Tagliata di tonno");
         menu[4].setPrezzo(12f);
         menu[5].setCategoria("secondo");
-        menu[5].setDescrizione("tagliata di manzo con verdure di contorno di stagione");
+        menu[5].setDescrizione("Tagliata di manzo con verdure di stagione");
         menu[5].setNome("Tagliata di manzo");
         menu[5].setPrezzo(10f);
         for(int i=1;i<6;i++){
             databaseHelper.addPietanza(menu[i]);
         }
-        titolo.setText("Ordine al tavolo "+tavolo+" dal cameriere "+utente);
+        titolo.setText("Ordine: tavolo "+tavolo+", cameriere "+utente);
         ordine.setCodice(databaseHelper.cercaUltimoCodiceOrdine()+1);
         databaseHelper.inserisciPietanze("INSERT OR IGNORE INTO pietanza (nome,categoria,descrizione,costo)\n" +
                 "VALUES "+
                 "" +
-                "('Tris di formaggi e affettati','antipasto','formaggi e affettati di stagione',9),\n" +
+                "('Misto di formaggi e affettati','antipasto','Formaggi stagionati, ricotta e affettati freschi della casa',9),\n" +
                 "('Vongole saltate con crostino','antipasto','vongole pescated oggi',6),\n" +
-                "('Antipasto vegetariano','antipasto','verdure di stagione',6),\n" +
-                "('Burrata con salmone affumicato e rucola','antipasto','il top della casa',10),\n" +
-                "('Crostata nostrana','dolce','crostata con marmellata fatta dalla casa',5),\n" +
-                "('Coppa gelato','dolce','gelato alla panna e alla fragola',4),\n" +
-                "('Profiteroll bianco','dolce','',4),\n" +
-                "('Profitterol nero','dolce','',4),\n" +
-                "('Birra bionda','bevanda','birra moretti',5),\n" +
-                "('Birra Rossa','bevanda','birra moretti rossa',5),\n" +
-                "('Vino rosso','bevanda','vino rosso della casa',4),\n" +
-                "('Vino bianco','bevanda','vino rosso della casa',4),\n" +
-                "('Acqua naturale','bevanda','bottiglia da 0.75',2.50),\n" +
-                "('Acqua gassata','bevanda','bottiglia da 0.75',2.50),\n" +
-                "('Caffe','bevanda','caffe classico',1.50),\n" +
-                "('Grappa della casa','bevanda','grappa fatta in casa',3);\n");
+                "('Antipasto vegetariano','antipasto','Mix di verdure cotte e crude di stagione',6),\n" +
+                "('Crostini al salmone','antipasto','Crostini con burro e salmone selvaggio affuicato',10),\n" +
+                "('Crostata','dolce','Crostata con marmellata fatta in casa',5),\n" +
+                "('Coppa gelato','dolce','Coppa con gelato alla panna e alla fragola',4),\n" +
+                "('Profiteroles bianco','dolce','Profiteroles al cioccolato bianco',4),\n" +
+                "('Profiteroles nero','dolce','Profiteroles al cioccolato al latte',4),\n" +
+                "('Birra bionda','bevanda','Birra Moretti bionda, 0.5 l',5),\n" +
+                "('Birra Rossa','bevanda','Birra Moretti rossa, 0.5 l',5),\n" +
+                "('Vino rosso','bevanda','Cabernet franc, 0.5 l',4),\n" +
+                "('Vino bianco','bevanda','Verduzzo, 0.5 l',4),\n" +
+                "('Acqua naturale','bevanda','Bottiglia, 0.75 l',2.50),\n" +
+                "('Acqua gassata','bevanda','Bottiglia, 0.75 l',2.50),\n" +
+                "('Caffè','bevanda','Caffè espresso',1.50),\n" +
+                "('Grappa','bevanda','Grappa fatta in casa',3);\n");
     }
 
     @Override
